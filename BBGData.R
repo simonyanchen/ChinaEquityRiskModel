cdd25089f1094587516ff093d46be6984ca55cc3
simonyanchen <wichenyn@gmail.com>
2017-01-23 17:53:18 +0800

    Data Process Update

--- BBGData.R
+++ BBGData.R
@@ -26,9 +26,25 @@ BBGData.Read.PX_LAST <-
     Options <- structure(c("NON_TRADING_WEEKDAYS","PREVIOUS_VALUE"), 
                          names = c("nonTradingDayFillOption","nonTradingDayFillMethod"))
     TEMP <- bdh(Universe$Ticker, "PX_LAST", start.date = S.Date, end.date = E.Date, options = Options)
-    DATE <- TEMP$`000001 CH Equity`$date
+    DATE <- bdh("SHSZ300 Index","PX_LAST", start.date = S.Date, end.date = E.Date, options = Options)$date
     PX_LAST <- cbind.data.frame(DATE,lapply(TEMP, (function(x) x$PX_LAST)))
     return(PX_LAST)
+  }
+
+BBGData.Read.EBITDA <-
+  function(Ref.Year)
+  {
+    #EBITDA = Operating Income + Depreciation & Amortization (+ Interest Expense)
+    OverRides <- structure(Ref.Year, names = "EQY_FUND_YEAR")
+    EBITDA <- bdp(Universe$Ticker, "EBITDA", overrides = OverRides)
+    OI <- bdp(Universe$Ticker, "IS_OPER_INC", overrides = OverRides)$IS_OPER_INC
+    DA <- bdp(Universe$Ticker, "CF_DEPR_AMORT", overrides = OverRides)$CF_DEPR_AMORT
+    IE <- bdp(Universe$Ticker, "IS_INT_EXPENSES", overrides = OverRides)$IS_INT_EXPENSES
+    Indx1 <- is.na(EBITDA$EBITDA)
+    Indx2 <- is.na(IE)
+    EBITDA$EBITDA[Indx1&Indx2] <- OI[Indx1&Indx2] + DA[Indx1&Indx2]
+    EBITDA$EBITDA[Indx1&!Indx2] <- OI[Indx1&!Indx2] + DA[Indx1&!Indx2] + IE[Indx1&!Indx2]
+    return(EBITDA)
   }
 
 BBGData.Initialize <-
